189b6783ce78142607b5a075bc68203cfef3c12f
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-12-05 17:39:21 +0100

    adjusted responsivity

--- src/components/Home/HomeNews.jsx
+++ src/components/Home/HomeNews.jsx
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getNews } from "../../redux/actions";
 import { Card, Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const HomeNews = () => {
   const news = useSelector(state => state.news.content.content);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function formatDate(dateString) {
     const options = { day: "2-digit", month: "2-digit", year: "numeric" };
@@ -24,10 +26,15 @@ const HomeNews = () => {
         LATEST NEWS
       </h3>
       {news && (
-        <Row>
+        <Row className="mb-5">
           {news.map((article, index) => (
-            <Col key={index} md={6} lg={4} xl={4} className="mb-4">
-              <Card className="bg-dark text-white">
+            <Col key={article.id} md={6} lg={4} xl={4} className={`mb-4 ${index >= 3 ? "d-none d-md-block" : ""}`}>
+              <Card
+                className="bg-black text-white cursor-pointer p-0"
+                onClick={() => {
+                  navigate("/news/" + article.id);
+                }}
+              >
                 <Card.Img src={article.game.gameCover} alt="Card image" className="rounded-0" />
                 <Card.ImgOverlay className="overlay rounded-0 d-flex flex-column justify-content-between">
                   <Card.Title className="text-truncate">{article.title}</Card.Title>
